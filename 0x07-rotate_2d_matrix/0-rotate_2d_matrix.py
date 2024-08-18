073cad7934c26421a8c21322c082ae3c3bea6062
Victor Vincent <dev.victovincent@gmail.com>
2024-08-18 16:25:17 +0000

    fix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,17 +3,15 @@
 0x07 - Rotate 2D Matrix
 """
 
+
 def rotate_2d_matrix(matrix):
     rows = len(matrix)
-    cols = len(matrix[0])
-    transposed_matrix = [[0] * rows for _ in range(cols)]
 
+    # Lets Transpose the matrix
+    for i in range(rows):
+        for j in range(i, rows):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-    for i in range (rows):
-        for j in range(cols):
-            transposed_matrix[j][i] = matrix[i][j]
-
-    for row in transposed_matrix:
-        row.reverse()
-
-    return transposed_matrix
+    # Lets Reverse each row of the matrix
+    for i in range(rows):
+        matrix[i].reverse()
