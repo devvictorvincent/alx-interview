b9bc24a0158cff5d7c2ca7639768162a1a14ed58
Victor Vincent <dev.victovincent@gmail.com>
2024-08-29 06:49:26 +0000

    fix

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,12 +6,9 @@ described in grid
 
 
 def island_perimeter(grid):
-     """Return the perimiter of an island.
+    """
+    Return the perimiter of an island.
     The grid represents water by 0 and land by 1.
-    Args:
-        grid (list): A list of list of integers representing an island.
-    Returns:
-        The perimeter of the island defined in grid.
     """
     if not grid or not grid[0]:
         return 0
